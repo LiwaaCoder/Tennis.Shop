deb7473b0322c0756fa3d2a4df24b6c24fba9f87
LiwaaHosh <66652532+LiwaaCoder@users.noreply.github.com>
2023-05-23 17:36:07 +0300

    Initial Commit

---
+++ Tennis.Shop/Tennis_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tennis_ShopApp.swift
+//  Tennis.Shop
+//
+//  Created by liwaa on 23/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Tennis_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
